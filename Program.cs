c50c08109066bcc3d282bd4bdb7d3507a1a8ef17
Parsa Karami <Parsakarami@live.com>
2024-02-02 12:51:15 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapGet("",() => "Use \"/Nerkh\" for currencies.");
+app.MapGet("/Nerkh", () => Random.Shared.Next(40000,60000).ToString());
 app.Run();
