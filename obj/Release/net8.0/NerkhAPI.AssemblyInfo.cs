eff12560576badd78e97343a4bf584e28a37c4e0
Parsa Karami <Parsakarami@live.com>
2024-02-02 13:20:06 -0500

    Add deployment files

--- obj/Release/net8.0/NerkhAPI.AssemblyInfo.cs
+++ obj/Release/net8.0/NerkhAPI.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("NerkhAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c50c08109066bcc3d282bd4bdb7d3507a1a8ef17")]
 [assembly: System.Reflection.AssemblyProductAttribute("NerkhAPI")]
